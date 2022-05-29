5a8461fd606a4dd2f98955bc8fe6bd92c974bf41
gustavo-mmh <gustavo-mmello@hotmil.com>
2022-05-29 20:17:37 -0300

    turning on the flash

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image, Alert, TouchableOpacity } from 'react-native';
 import imagemL from './assets/icons/logo-palmeiras-claro.png';
 import imagemD from './assets/icons/logo-palmeiras-escuro.png';
+import Torch from 'react-native-torch';
 const App = () => {
   const [toggle, setToggle] = useState(false);
   const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
   useEffect(() => {
-    Alert.alert('Avanti Palestra!!!');
-  });
+    Torch.switchState(toggle)
+    console.log("trocou estado do flash");
+  }, [toggle]);
 
   return (
     < View style={toggle ? style.containerLight : style.container} >
