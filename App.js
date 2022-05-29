e07dff6e0d41b3ec7bd9d4b6ba5eeb613e25835b
gustavo-mmh <gustavo-mmello@hotmil.com>
2022-05-29 19:19:46 -0300

    acareate ChangeToggle

--- App.js
+++ App.js
@@ -1,111 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Image, Alert, TouchableOpacity } from 'react-native';
+import imagemL from './assets/icons/logo-palmeiras-claro.png';
+import imagemD from './assets/icons/logo-palmeiras-escuro.png';
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+  useEffect(() => {
+    Alert.alert('Avanti Palestra!!!');
+  });
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    < View style={toggle ? style.containerLight : style.container} >
+      <TouchableOpacity onPress={handleChangeToggle}>
+
+        <Image
+          style={toggle ? style.LightingOn : style.LightingOff}
+          source={toggle ? imagemL : imagemD}
+        />
+
+      </TouchableOpacity>
+
+    </View >
+  )
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#006135",
+    alignItems: "center",
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  containerLight: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  LightingOn: {
+    resizeMode: "contain",
+    alignSelf: "center",
+    width: 300,
+    height: 300,
   },
-  highlight: {
-    fontWeight: '700',
+  LightingOff: {
+    resizeMode: "contain",
+    alignSelf: "center",
+    width: 300,
+    height: 300,
   },
 });
 
